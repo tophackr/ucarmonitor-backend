8b34fedcada57382e84b217e01b53b0a04a7f59c
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-04-23 13:50:16 +0300

    refactor: simplify user retrieval in decorators and update TelegramStrategy return types

--- src/auth/decorators/user.decorator.ts
+++ src/auth/decorators/user.decorator.ts
@@ -4,7 +4,7 @@ import { User } from '@prisma/client'
 export const CurrentUser = createParamDecorator(
     (data: keyof User, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest()
-        const user = request.user.user
+        const user = request.user
         user['id'] = String(user['id'])
 
         return data ? user[data] : user
--- src/auth/strategy/telegram.strategy.ts
+++ src/auth/strategy/telegram.strategy.ts
@@ -1,11 +1,9 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { PassportStrategy } from '@nestjs/passport'
-import { InitData, parse, validate } from '@telegram-apps/init-data-node'
+import { parse, User, validate } from '@telegram-apps/init-data-node'
 import { Request } from 'express'
 import { Strategy } from 'passport-custom'
-
-type InitDataResponse = Pick<InitData, 'auth_date' | 'query_id' | 'user'>
 
 @Injectable()
 export class TelegramStrategy extends PassportStrategy(Strategy, 'telegram') {
@@ -13,7 +11,7 @@ export class TelegramStrategy extends PassportStrategy(Strategy, 'telegram') {
         super()
     }
 
-    private validateInitData(initData: string): InitDataResponse | null {
+    private validateInitData(initData: string): User {
         try {
             validate(initData, this.configService.get('TELEGRAM_TOKEN'), {
                 expiresIn: 3600
@@ -23,12 +21,12 @@ export class TelegramStrategy extends PassportStrategy(Strategy, 'telegram') {
             return null
         }
 
-        const { auth_date, query_id, user } = parse(initData)
+        const { user } = parse(initData)
 
-        return { auth_date, query_id, user }
+        return user
     }
 
-    async validate(request: Request): Promise<InitDataResponse> {
+    async validate(request: Request): Promise<User> {
         const initData = request.headers['x-telegram-data'] as string
 
         if (!initData) {
@@ -40,13 +38,9 @@ export class TelegramStrategy extends PassportStrategy(Strategy, 'telegram') {
 
         if (isDev && initData.includes('start_param=debug')) {
             return {
-                auth_date: new Date(),
-                query_id: crypto.randomUUID(),
-                user: {
-                    id: 123,
-                    username: 'example',
-                    first_name: 'example'
-                }
+                id: 123,
+                username: 'example',
+                first_name: 'example'
             }
         }
 
